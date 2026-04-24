43e2a734c5b4c30ab2d11c465ac01129fbf03c3d
317110375 <317110375@pcpuma.acatlan.unam.mx>
2026-04-24 04:10:07 +0000

    feat: definición de variables de datos y estructura base del Header

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,1 +1,11 @@
-export default function Header() { return null; }
+import React from 'react';
+import styles from './Header.module.css';
+
+let HeaderData: string = 'Diego Romero Mora';
+
+export default function Header() {
+  return (
+    <>
+    </>
+  );
+}
