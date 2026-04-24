9141a60dc54c28e73b51cfae643d83fff1106983
317110375 <317110375@pcpuma.acatlan.unam.mx>
2026-04-24 04:10:20 +0000

    feat: importación y renderizado del componente Header en la Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
+import HeaderModule from './Components/Header/Header';
 
 export default function Home() {
   return (
     <>
+      <HeaderModule />
     </>
   );
 }
